1c532d7f2a4f75fd73ebf3e2e8bece8d85a6e239
kyleb-herts <kb20acu@herts.ac.uk>
2021-03-27 19:44:02 +0000

    Change sort order

--- sentimental-analysis.py
+++ sentimental-analysis.py
@@ -6,7 +6,7 @@
 import pandas as pd
 from afinn import Afinn
 
-afinn = Afinn(language='en')
+afinn = Afinn(language='en', emoticons=True)
 
 pd.set_option('display.max_rows', None)
 
@@ -14,13 +14,14 @@ data = pd.read_csv("reviews_pseudoanonymised.csv")
 data['afinn_score'] = data['comment'].apply(afinn.score)
 
 columns_to_display = ['comment', 'afinn_score']
-filtered_data = data.sort_values(by='afinn_score')[columns_to_display]
+filtered_data = data.sort_values(by='afinn_score', ascending=False)[columns_to_display]
 
-print("\n")
-print(data.sort_values(by='afinn_score')[columns_to_display])
-print("\n")
-print("Afinn Score From Comments Summary")
-print(data['afinn_score'].describe())
+#print("\n")
+#print(filtered_data)
+#print("\n")
+#print("Afinn Score From Comments Summary")
+#print(data['afinn_score'].describe())
 
 filtered_data.to_csv('reviews_pseudoanonymised_afinn_score_applied.csv')
 data['afinn_score'].describe().to_csv('afinn_score_summary.csv')
+print("Sentiment Analyis completed - AFINN score applied")
