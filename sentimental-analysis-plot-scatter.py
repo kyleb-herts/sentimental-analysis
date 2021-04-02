ed6261e7d8f08e0b071a8685fed1790a757f7473
Brian Leung <brian.leungkp@gmail.com>
2021-04-02 22:13:41 +0800

    Add comment helpful calculation

--- sentimental-analysis-plot-scatter.py
+++ sentimental-analysis-plot-scatter.py
@@ -7,16 +7,30 @@ import pandas as pd
 from afinn import Afinn
 import matplotlib.pyplot as plt
 
+
+def modify_helpful_value(text_string):
+    # NaN value reurn 1.0
+    if text_string != text_string:
+        return 1.0
+    else:
+        return text_string
+
+
 afinn = Afinn(language='en', emoticons=True)
 
 pd.set_option('display.max_rows', None)
 
 data = pd.read_csv("reviews_pseudoanonymised.csv")
+data['helpful'] = data['helpful'].apply(modify_helpful_value)
 data['afinn_score'] = data['comment'].apply(afinn.score)
 
 d = {"comment":data['afinn_score'].index.tolist(), "score":data['afinn_score'].array}
 df = pd.DataFrame(data=d)
 df.plot.scatter(x="comment", y="score")
+plt.show()
+d2 = {"comment":data['afinn_score'].index.tolist(), "helpful":data['helpful'].array}
+df2 = pd.DataFrame(data=d2)
+df2.plot.scatter(x="comment", y="helpful")
 plt.show()
 # df = pd.DataFrame({"x":keys, "score":values})
 # ax = df.plot.scatter(x='x', y='score')
